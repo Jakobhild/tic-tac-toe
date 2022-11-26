aa6bea4fdec5cd50990ea49c3bf08307ccdb8bac
Jakobhild <30655321+Jakobhild@users.noreply.github.com>
2022-11-26 21:35:03 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class app extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Tic Tac Toe</h1>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>
 );
 
